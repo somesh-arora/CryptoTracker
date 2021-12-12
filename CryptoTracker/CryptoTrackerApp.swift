18f2b04ea3983c3680a5c1f5d2a9a761a4ff9061
Somesh Arora <somesh.arora@microsoft.com>
2021-12-12 13:46:53 -0800

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Somesh Arora on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
