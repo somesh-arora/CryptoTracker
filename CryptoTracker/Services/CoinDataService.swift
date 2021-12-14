ec18673ef3c133e862b2e93bf3a1f4681638fa21
Somesh Arora <somesh.arora@microsoft.com>
2021-12-13 22:50:01 -0800

    Added Reusable Network Logic

--- CryptoTracker/Services/CoinDataService.swift
+++ CryptoTracker/Services/CoinDataService.swift
@@ -24,27 +24,12 @@ class CoinDataService {
             return
         }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { output -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                          throw URLError(.badServerResponse)
-                      }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] coins in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion,
+                  receiveValue: { [weak self] coins in
                 self?.allCoins = coins
                 self?.coinSubscription?.cancel()
-            }
+            })
     }
 }
