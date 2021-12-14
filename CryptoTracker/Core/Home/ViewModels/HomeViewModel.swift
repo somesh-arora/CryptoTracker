e326e8fb112636b737c0663fb64223451a47146d
Somesh Arora <somesh.arora@microsoft.com>
2021-12-13 22:04:49 -0800

    Add CoinDataService to get data from API

--- CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -13,10 +13,19 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    private var cancellables = Set<AnyCancellable>()
+    
+    private let dataService = CoinDataService()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] coins in
+                self?.allCoins = coins
+            }
+            .store(in: &cancellables)
     }
 }
