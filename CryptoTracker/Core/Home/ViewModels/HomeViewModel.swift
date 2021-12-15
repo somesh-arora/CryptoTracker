f6c042bc9a44bcbd48803b2933acabd2f1e73ddf
Somesh Arora <somesh.arora@microsoft.com>
2021-12-14 22:33:05 -0800

    Filtering coins based on search TextField

--- CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,7 +12,9 @@ class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    
     @Published var searchText: String = ""
+    
     private var cancellables = Set<AnyCancellable>()
     
     private let dataService = CoinDataService()
@@ -22,10 +24,28 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filteredCoins)
             .sink { [weak self] coins in
                 self?.allCoins = coins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filteredCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText)
+        }
     }
 }
